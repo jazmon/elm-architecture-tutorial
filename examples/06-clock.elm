b12869b75e0a5ef7ee5af7f85ba1d9cf0ce13fb7
Atte Huhtakangas <atte.huhtakangas@gmail.com>
2016-12-26 21:42:07 +0200

    progress

--- examples/06-clock.elm
+++ examples/06-clock.elm
@@ -1,9 +1,11 @@
 module Main exposing (..)
 
-import Html exposing (Html)
+import Html exposing (Html, div, p)
+import Html.Attributes
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
 import Time exposing (Time, second)
+import Date exposing (fromTime)
 
 
 main : Program Never Model Msg
@@ -57,8 +59,38 @@ subscriptions model =
 -- VIEW
 
 
-view : Model -> Html Msg
-view model =
+hourLine : Model -> List (Attribute msg)
+hourLine model =
+    let
+        angle =
+            turns (Time.inHours model) / 12
+
+        handX =
+            toString (50 + 40 * cos angle)
+
+        handY =
+            toString (50 + 40 * sin angle)
+    in
+        [ x1 "50", y1 "50", x2 handX, y2 handY, stroke "#000f1a" ]
+
+
+minuteLine : Model -> List (Attribute msg)
+minuteLine model =
+    let
+        angle =
+            turns (Time.inHours model)
+
+        handX =
+            toString (50 + 40 * cos angle)
+
+        handY =
+            toString (50 + 40 * sin angle)
+    in
+        [ x1 "50", y1 "50", x2 handX, y2 handY, stroke "#023963" ]
+
+
+secondLine : Model -> List (Attribute msg)
+secondLine model =
     let
         angle =
             turns (Time.inMinutes model)
@@ -69,7 +101,55 @@ view model =
         handY =
             toString (50 + 40 * sin angle)
     in
-        svg [ viewBox "0 0 100 100", width "300px" ]
-            [ circle [ cx "50", cy "50", r "45", fill "#0B79CE" ] []
-            , line [ x1 "50", y1 "50", x2 handX, y2 handY, stroke "#023963" ] []
+        [ x1 "50", y1 "50", x2 handX, y2 handY, stroke "#cb1122" ]
+
+
+timeView : Model -> Html msg
+timeView model =
+    div []
+        [ p [] [ text <| getHour model ++ ":" ++ getMinute model ++ ":" ++ getSecond model ] ]
+
+
+view : Model -> Html Msg
+view model =
+    let
+        radius =
+            50
+    in
+        div
+            [ Html.Attributes.style
+                [ ( "margin", "1rem" )
+                , ( "display", "flex" )
+                , ( "height", "100%" )
+                , ( "flex-direction", "column" )
+                , ( "align-items", "center" )
+                , ( "justify-content", "center" )
+                ]
             ]
+            [ svg [ viewBox "0 0 100 100", width "300px", Html.Attributes.style [ ( "flex-grow", "0" ) ] ]
+                [ circle [ cx <| toString radius, cy <| toString radius, r <| toString radius, fill "#0B79CE" ] []
+                , line (hourLine model) []
+                , line (secondLine model) []
+                , line (minuteLine model) []
+                ]
+            , timeView model
+            ]
+
+
+
+-- utils
+
+
+getHour : Model -> String
+getHour model =
+    toString <| Date.hour <| fromTime model
+
+
+getMinute : Model -> String
+getMinute model =
+    toString <| Date.minute <| fromTime model
+
+
+getSecond : Model -> String
+getSecond model =
+    toString <| Date.second <| fromTime model
